0300662b7a8fd7c59e8015085519802fca09c1f2
chriskilinc <chriskilinc@gmail.com>
2018-01-02 16:38:46 +0100

    Implemented a fetch request to a wp rest-api to return an article with a specified ID

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Hero from './Components/Hero';
+
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      article: [],
+    }
+
+  }
+
+  componentDidMount = () => {
+    //this.getPostById(4);
+    this.getArticleById(4);
+  }
+
+
+
+  getArticleById = (id) => {
+    fetch("http://wp.elknappen.se/wp-json/wp/v2/posts/" + id)
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Network request failed")
+        }
+        return response.json()
+      })
+      .then(data => {
+        this.setState({
+          article: data
+        })
+        console.log(this.state.article.title.rendered);
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Hero articleId={this.state.article.id} articleTitle={this.state.article.title.rendered}/>
       </div>
     );
   }
