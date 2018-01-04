7e219a77eadc4238a243284757c5a90789d30d85
chriskilinc <chriskilinc@gmail.com>
2018-01-04 20:05:21 +0100

    Came to my senses that I don't really need an API at this moment

--- src/App.js
+++ src/App.js
@@ -6,75 +6,11 @@ import Hero from './Components/Hero';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      article: {
-        title:{
-          rendered: ''
-        },
-        content:{
-          rendered: ''
-        },
-        id: undefined,
-      },
-    }
-
-    
-
-  }
-
-  componentDidMount = () => {
-    //this.getPostById(4);
-    this.getArticleById(4);
-    
-  }
-
-  getArticleById = (id) => {
-    fetch("http://wp.elknappen.se/wp-json/wp/v2/posts/" + id)
-      .then(response => {
-        if (!response.ok) {
-          throw Error("Network request failed")
-        }
-        return response.json()
-      })
-      .then(data => {
-
-        
-        data.content.rendered = this.cleanHtml(data.content.rendered);
-        data.content.rendered = this.toUnicode(data.content.rendered);
-
-        //let newContent = data.content.rendered.replace(/<\/?[^>]+(>|$)/g, "");
-        //data.content.rendered = newContent;
-
-        this.setState({
-          article: data
-        })
-      })
-  }
-
-  cleanHtml = (dirtyContent) => {
-    let cleanedContent = dirtyContent.replace(/<\/?[^>]+(>|$)/g, "");
-    return cleanedContent;
-  }
-
-  //  Doesnt do what i want
-  toUnicode= (str) => {
-    return str.split('').map(function (value, index, array) {
-      var temp = value.charCodeAt(0).toString(16).toUpperCase();
-      if (temp.length > 2) {
-        return '\\u' + temp;
-      }
-      return value;
-    }).join('');
-  }
-  
-
   render() {
     return (
       <div className="App">
 
-        <Hero articleId={this.state.article.id} articleTitle={this.state.article.title.rendered} articleContent={this.state.article.content.rendered}/>
+        < Hero />
       </div>
     );
   }
