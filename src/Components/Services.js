768fd820408a66b42c13c78d9e7ab5b22399a9fb
chriskilinc <chriskilinc@gmail.com>
2018-01-12 20:43:27 +0100

    Added Service component for the Service Section and Styling.

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
 import '../css/services.css';
 
+import img from '../svg/image.svg';
+import shapes from '../svg/shapes.svg';
+import tele from '../svg/phone-call.svg';
+import idea from '../svg/idea.svg';
+
+
 class Services extends Component {
 
   render() {
     return (
-      <section id="services" className="services">
-        <h1>Services</h1>
+      <section id="services" className="col-12 services">
+        <div className="contents">
+          <h1>Tjänster</h1>
+          <div className="contents-container">
+            <p>Vi Erbjuder Dessa Tjänster</p>
+            <div className="service-cards row">
+
+              <div className="service-card col-12 col-lg-4">
+                <div className="service-card-container">
+                  <div className="service-card-content">
+                    <img src={idea} alt="Lampbulb" className="img-fluid" />
+                    <h1>EL</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod felis non fringilla rhoncus. In consequat lorem eget tempor ultrices. Nunc et posuere libero. </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="service-card col-12 col-lg-4">
+                <div className="service-card-container">
+                  <div className="service-card-content">
+                    <img src={tele} alt="Phone" className="img-fluid" />
+                    <h1>TELE</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod felis non fringilla rhoncus. In consequat lorem eget tempor ultrices. Nunc et posuere libero. </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="service-card col-12 col-lg-4">
+                <div className="service-card-container">
+                  <div className="service-card-content">
+                    <img src={shapes} alt="network" className="img-fluid" />
+                    <h1>NÄTVERK</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod felis non fringilla rhoncus. In consequat lorem eget tempor ultrices. Nunc et posuere libero. </p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
       </section>
     );
   }
