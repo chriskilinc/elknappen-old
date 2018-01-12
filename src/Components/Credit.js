2dcbbf06da0ebbcd973f2e37027b20f31fd73fd7
chriskilinc <chriskilinc@gmail.com>
2018-01-12 20:40:30 +0100

    Added a Credit Component and styling

---
+++ src/Components/Credit.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import '../css/credit.css';
+
+class Credit extends Component {
+	render() {
+		return (
+			<section className="credit">
+				<h1>Icon made by Freepik from https://www.flaticon.com/authors/freepik</h1>
+        <a href="https://www.flaticon.com/free-icon/idea_122921#term=electricity">idea.svg</a>
+        <a href="https://www.flaticon.com/free-icon/phone-call_114333#term=phone">phone-call.svg</a>
+        
+        <h1>Icon made by Smashicons from https://www.flaticon.com/authors/smashicons</h1>
+        <a href="https://www.flaticon.com/free-icon/network_149181"> Network.svg</a>
+			</section>
+      
+    );
+    
+	}
+}
+
+export default Credit;
