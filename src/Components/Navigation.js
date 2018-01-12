e4f82fedaa73fade5c3b8a7a25ba0478e59f6639
chriskilinc <chriskilinc@gmail.com>
2018-01-12 14:04:02 +0100

    Some styling changes to Navigation.js

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -8,7 +8,7 @@ class Navigation extends Component {
       <nav className="navigation">
         <div className="nav-wrapper">
           <div className="nav-container container-fluid row">
-            <div className="nav-logo col-md-4 col-12 container-fluid row">
+            <div className="nav-logo col-md-5 col-lg-4 col-12 container-fluid row">
               <div className="col-8">
                 <h1>Elknappen</h1>
               </div>
@@ -22,7 +22,7 @@ class Navigation extends Component {
               </div>
 
             </div>
-            <div className="nav-menu col-md-8 row">
+            <div className="nav-menu col-md-7 col-lg-8 row">
               <div className="nav-menu-container">
                 <a href="#">Hem</a>
                 <a href="#">Tjänster</a>
