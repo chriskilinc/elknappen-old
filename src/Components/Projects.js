fdaf25f1122e19ee584b67f1a7f72c80db02aca9
chriskilinc <chriskilinc@gmail.com>
2018-01-22 16:03:49 +0100

    Added projects component and styling

---
+++ src/Components/Projects.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../css/projects.css';
+
+
+class Projects extends Component {
+
+  render() {
+    return (
+      <section id="projects" className="col-12 projects">
+        <div className="contents">
+          <h1>Projekt</h1>
+          <div className="contents-container">
+            <p>Våra Projekt</p>
+            
+          </div>
+        </div>
+
+      </section>
+    );
+  }
+}
+
+export default Projects;
