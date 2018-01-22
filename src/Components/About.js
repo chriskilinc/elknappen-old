9834a229e2b923bd75843e6ffcc0f352da049fd8
chriskilinc <chriskilinc@gmail.com>
2018-01-22 16:04:31 +0100

    Added About component and styling

---
+++ src/Components/About.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../css/about.css';
+
+
+class About extends Component {
+
+  render() {
+    return (
+      <section id="about" className="col-12 about">
+        <div className="contents">
+          <h1>Om Oss</h1>
+          <div className="contents-container">
+            <p>Information om Elknappen</p>
+            
+          </div>
+        </div>
+
+      </section>
+    );
+  }
+}
+
+export default About;
