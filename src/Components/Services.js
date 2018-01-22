24d08d91336e46c37d2f6fdfd738bfa8893206fd
chriskilinc <chriskilinc@gmail.com>
2018-01-22 14:44:44 +0100

    Updated font size on everything in the project. Making it smaller by some units.

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -5,6 +5,7 @@ import img from '../svg/image.svg';
 import shapes from '../svg/shapes.svg';
 import tele from '../svg/phone-call.svg';
 import idea from '../svg/idea.svg';
+import smarthome from '../svg/smartbulb.svg';
 
 
 class Services extends Component {
@@ -18,7 +19,7 @@ class Services extends Component {
             <p>Vi Erbjuder Dessa Tjänster</p>
             <div className="service-cards row">
 
-              <div className="service-card col-12 col-lg-4">
+              <div className="service-card col-12 col-lg-3">
                 <div className="service-card-container">
                   <div className="service-card-content">
                     <img src={idea} alt="Lampbulb" className="img-fluid" />
@@ -28,7 +29,17 @@ class Services extends Component {
                 </div>
               </div>
 
-              <div className="service-card col-12 col-lg-4">
+              <div className="service-card col-12 col-lg-3">
+                <div className="service-card-container">
+                  <div className="service-card-content">
+                    <img src={smarthome} alt="Lampbulb" className="img-fluid" />
+                    <h1>Smart Home</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod felis non fringilla rhoncus. In consequat lorem eget tempor ultrices. Nunc et posuere libero. </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="service-card col-12 col-lg-3">
                 <div className="service-card-container">
                   <div className="service-card-content">
                     <img src={tele} alt="Phone" className="img-fluid" />
@@ -38,7 +49,7 @@ class Services extends Component {
                 </div>
               </div>
 
-              <div className="service-card col-12 col-lg-4">
+              <div className="service-card col-12 col-lg-3">
                 <div className="service-card-container">
                   <div className="service-card-content">
                     <img src={shapes} alt="network" className="img-fluid" />
